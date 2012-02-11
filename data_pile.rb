65516602fdec8a920fa1e68680d997daf9b1d373
BraggingBoozer <bragging.boozer@gmail.com>
2012-02-12 02:09:11 +0900

    Create FilePile class.
    The data pile class based on files in a directory is created.

--- data_pile.rb
+++ data_pile.rb
@@ -20,6 +20,8 @@
 #You should have received a copy of the GNU General Public License along with
 #this program; if not, see <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>.
 
+require 'find'
+
 #= The module which defines interfaces for data piles.
 #
 #Every data pile classes must include this module and override methods.
@@ -53,7 +55,6 @@ end
 #
 #User of data pile must define class for records in the pile and include this module in it.
 module Record
-
   #Key of injected data(The data pile uses this when recoed is discarded).
   attr_reader :element_key
 
@@ -68,5 +69,58 @@ module Record
   #The data pile can inject a raw data to a record by this method.
   def inject(key, element)
     @element_key = key
+  end
+end
+
+#= File based data pile class.
+#
+#Data pile based files in a directory.
+class FilePile
+  include Pile
+
+  #== Initializer
+  #
+  #=== Parameters
+  #dir:: pile directory
+  #=== Note
+  #Create data pile with specified directory.
+  def initialize(dir, klass)
+    @dir = dir
+
+    if @dir !~ /\/$/
+      @dir += "/"
+    end
+
+    @record_class = klass
+  end
+
+  #== Fetch a record
+  #
+  #=== Parameters
+  #none
+  #=== Returns
+  #a record
+  #=== Note
+  #Pick up file and create record with it.
+  def fetch()
+    record = @record_class.new
+
+    Find.find(@dir).select{|f| FileTest.file?(f)}.each do |f|
+      File.open(f) do |fo|
+        record.inject(File.basename(f), fo)
+      end
+    end
+  end
+
+  #== Discard a record
+  #
+  #=== Parameters
+  #record:: The record which should be deleted.
+  #=== Reruens
+  #none
+  #=== Note
+  #Remove an injected file with specified record.
+  def discard(record)
+    File.unlink(@dir + record.element_key)
   end
 end
